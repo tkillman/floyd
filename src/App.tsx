c25e975232273743c104d77f7e1cb11a38b8f69f
kimdonggyu <dkkim1004@hanmail.net>
2024-09-06 17:32:27 +0900

    lazy 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { RoutePath } from './domain/route.domain';
-import { DijkstraPage, WelcomePage } from './page';
+import { lazy } from 'react';
+
+const LazyWelcomePage = lazy(() => import('./page/WelcomePage'));
+const LazyDijkstraPage = lazy(() => import('./page/DijkstraPage'));
 
 const App = () => {
   return (
     <RecoilRoot>
       <BrowserRouter>
         <Routes>
-          <Route path={RoutePath.WELCOME} element={<WelcomePage />} />
-          <Route path={RoutePath.DI} element={<DijkstraPage />} />
+          <Route path={RoutePath.WELCOME} element={<LazyWelcomePage />} />
+          <Route path={RoutePath.DI} element={<LazyDijkstraPage />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
--- src/page/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WelcomePage } from './WelcomePage';
-export { default as DijkstraPage } from './DijkstraPage';
