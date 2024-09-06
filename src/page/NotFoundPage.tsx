6857e87265f8c0c2add254c60d2aa02e2ef3f1d5
kimdonggyu <dkkim1004@hanmail.net>
2024-09-07 04:28:39 +0900

    404 page change

--- src/page/NotFoundPage.tsx
+++ src/page/NotFoundPage.tsx
@@ -1,12 +1,17 @@
-import { RoutePath } from '../domain/route.domain';
+import { RoutePath, routePathName } from '../domain/route.domain';
 import { NotFoundPageWrapper } from './NotFoundPage.style';
 
 const NotFoundPage = () => {
   return (
     <NotFoundPageWrapper>
-      <a href={RoutePath.WELCOME}>메인 이동 (플로이드 워셜 알고리즘 간편)</a>
-      <a href={RoutePath.FL}>플로이드 워셜 알고리즘(방향 존재)</a>
-      <a href={RoutePath.DI}>다익스트라 알고리즘</a>
+      <a href="/">메인이동</a>
+      {Object.values(RoutePath).map((path) => {
+        return (
+          <a key={path} href={path}>
+            {routePathName(path)}
+          </a>
+        );
+      })}
     </NotFoundPageWrapper>
   );
 };
