3ed18d414ffd5d56efd18effa1c947ecf6ec7403
kimdonggyu <dkkim1004@hanmail.net>
2024-10-19 17:00:55 +0900

    빌드오류수정

--- src/ui/page/ProfessorPage.tsx
+++ src/ui/page/ProfessorPage.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import CanvasComponent, { RefCanvasComponent } from '../common/CanvasComponent';
-import useDevice from '~/src/lib/device';
 import { PageWrapper } from './common/page.style';
-import useDrawFixDirection, {
-  NODE_COUNT,
-} from '~/src/application/service/canvas/drawFixDirection.service';
+import useDrawFixDirection from '~/src/application/service/canvas/drawFixDirection.service';
 import ProfessorComponent from '../professor/ProfessorComponent';
 
 const ProfessorPage = () => {
